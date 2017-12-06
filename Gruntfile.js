2de0da3b20c2e3c7c7eb4745dcc52cdecfb70a7d
Chandler Morrison <cwm795@gmail.com>
2017-12-06 15:00:02 -0600

    inital commit/grunt set up

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+// import {  read } from "fs";
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    browserify: {
+      "dist/bundle.js": ["javascripts/main.js"]
+    },
+    jshint: {
+      files: ["javascripts/**/*.js"],
+      options: {
+        predef: ["document", "console"],
+        esnext: true,
+        globalstrict: true,
+        globals: {},
+        browserify: true
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          "stylesheets/main.css": "sass/main.scss"
+        }
+      }
+    },
+    watch: {
+      javascripts: {
+        files: ["javascripts/**/*.js"],
+        task: ["jshint", "browserify"]
+      },
+      sass: {
+        files: ["sass/**/*.scss"],
+        tasks: ["sass"]
+      }
+    }
+  });
+
+  require("matchdep")
+    .filter("grunt-*")
+    .forEach(grunt.loadNpmTasks);
+
+  grunt.registerTask("default", ["jshint", "sass", "browserify", "watch"]);
+};
